c5cf2338705d2395a8286709c868714c66f3013b
fardous hridoy <mdreday736@gmail.com>
2022-05-25 00:24:51 +0600

    card color added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen bg-base-200">
+        <div class="hero min-h-screen ">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img className='rounded-lg' width={604} height={541} src="https://img.freepik.com/free-photo/handyman-uses-jackhammer-installation-professional-worker-construction-site-concept-electrician-handyman_169016-5881.jpg?t=st=1653399951~exp=1653400551~hmac=3aa7ff0cb235f40599214895a099e2979aadb8e44cf065e7947438383129427a&w=1060" />
                 <div>
--- src/Pages/Home/Info/Info.js
+++ src/Pages/Home/Info/Info.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import InfoCard from './InfoCard';
 import clock from '../../../icons/clock.svg';
+import marker from '../../../icons/marker.svg';
+import phone from '../../../icons/phone.svg';
 
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={clock}></InfoCard>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+            <InfoCard cardTitle="Opening Hours" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>
+            <InfoCard cardTitle="Our Locations" bgClass="bg-accent" img={marker}></InfoCard>
+            <InfoCard cardTitle="Contract Us" bgClass="bg-gradient-to-r from-secondary to-primary" img={phone}></InfoCard>
         </div>
     );
 };
